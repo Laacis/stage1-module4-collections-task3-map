73f3e0f171192f95f8af88f1971538ef59c41178
Jurijs <j.lacis.code@gmail.com>
2024-01-04 13:19:26 +0800

    complete

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> results = new HashMap<>();
+
+        for(Map.Entry<Integer, String> entry : sourceMap.entrySet()){
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            Integer resultsKey = results.get(value);
+
+            if(results.containsKey(value)){
+                if(key < resultsKey){
+                    results.put(value, key);
+                }
+            }else {
+                results.put(value,key);
+            }
+        }
+
+        return  results;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> results = new HashMap<>();
+        StringTokenizer tokenizer = new StringTokenizer(sentence, " ,.");
+
+        while(tokenizer.hasMoreTokens()){
+            String word = tokenizer.nextToken();
+            word = word.toLowerCase();
+            Integer val = results.get(word);
+
+            if( val == null){
+                results.put(word, 1);
+            }else {
+                results.put(word, ++val);
+            }
+        }
+
+        return results;
     }
 }
